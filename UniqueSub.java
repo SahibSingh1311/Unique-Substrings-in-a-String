c73378f77052b2126e90391185974bf5838ddb56
Sahib Singh <51374188+SahibSingh1311@users.noreply.github.com>
2022-08-23 16:48:13 +0530

    Add files via upload
    Unique Sub String using HashSet

--- UniqueSub.java
+++ UniqueSub.java
@@ -1,19 +1,19 @@
+//Using HashSet
 import java.util.*;
 
 public class UniqueSub{
 	public static void main(String[] args){
-		String s1= "abc";
+		String s1= "banana";
 		System.out.println(uniqueSubstring(s1));
 }
 
-	public static ArrayList<String> uniqueSubstring(String str){
-	ArrayList<String> substrs=new ArrayList<String>();
+	public static HashSet<String> uniqueSubstring(String str){
+	HashSet<String> substrs=new HashSet<String>();
 	for(int i = 0; i<str.length();i++){
 		for(int j=0;j<str.length()-i;j++){
 			String sub = str.substring(j,j+(i+1));
-			if(!substrs.contains(sub)){
-				 substrs.add(sub);
-}
+			 substrs.add(sub);
+
 }
 }
 	return substrs;	
